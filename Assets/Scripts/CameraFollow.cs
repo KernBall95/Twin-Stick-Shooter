5a7dc29e36aa5fb799c1ad865fe53ab2b1dd81b6
Kern Ball <kern.ball@yahoo.co.uk>
2018-12-20 16:25:15 +0000

    Player Movement and Camera Follow
    The first iteration of player movement is complete and the camera now follows the player.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform player;
+    public float followSpeed;
+
+	void Update () {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(player.position.x, transform.position.y, player.position.z), followSpeed);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float moveSpeed;
+
+    Vector3 movementDirection;
+    float xAxis, zAxis;
+    Rigidbody rb;
+	
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+	void Update () {
+        xAxis = Input.GetAxis("Horizontal");
+        zAxis = Input.GetAxis("Vertical");
+
+        movementDirection = new Vector3(xAxis, 0, zAxis);
+        rb.velocity = movementDirection * moveSpeed;
+    }
+}
