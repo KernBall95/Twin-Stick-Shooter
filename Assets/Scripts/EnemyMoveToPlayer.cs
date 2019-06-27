3f7343bdc7754a11d30bea0b1c6e5b3e4140a086
Kern Ball <kern.ball@yahoo.co.uk>
2019-06-27 11:56:11 +0100

    Enemy Type 1
    Added enemies that move towards the player. They do not yet damage the player.

---
+++ Assets/Scripts/EnemyMoveToPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMoveToPlayer : MonoBehaviour {
+
+    public GameObject target;
+
+    private NavMeshAgent agent;
+
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        target = GameObject.Find("Temp Player");
+        agent.speed = Random.Range(16, 20);
+	}
+	
+	void Update () {
+        agent.destination = target.transform.position;
+	}
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+
+    public Transform[] enemySpawnsArray;
+    public int spawnCount;
+    public GameObject enemy;
+    public float spawnRate;
+
+	void Start () {
+        FindSpawnPoints();
+        StartCoroutine(SpawnEnemies());
+	}
+	
+	void Update () {
+		
+	}
+
+    void FindSpawnPoints()
+    {
+        enemySpawnsArray = new Transform[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            enemySpawnsArray[i] = transform.GetChild(i);
+            Debug.Log(enemySpawnsArray[i]);
+        }
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        for(int i = 0; i < spawnCount; i++)
+        {
+            Instantiate(enemy, enemySpawnsArray[Random.Range(0, transform.childCount - 1)].position, Quaternion.identity);
+            yield return new WaitForSeconds(spawnRate);
+        }
+    }
+}
